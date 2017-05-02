f3236052332c08d0483ba73eea63a3529eaa57e5
Andreza Medeiros <andreza.cmedeiros@gmail.com>
2017-05-02 01:00:46 -0300

    changes actors list on smile compiler to hash map

--- src/main/java/br/ufrn/smile/SmileCompiler.java
+++ src/main/java/br/ufrn/smile/SmileCompiler.java
@@ -3,6 +3,7 @@ package br.ufrn.smile;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import com.thoughtworks.xstream.XStream;
@@ -12,34 +13,34 @@ import br.ufrn.smile.domain.CustomError;
 import br.ufrn.smile.domain.ErrorHandler;
 
 public class SmileCompiler {
-	private List<ActorStatementFactory> actors;
+	private HashMap<String, ActorStatementFactory> actors;
 	
 	public SmileCompiler() {
-		this.actors = new ArrayList<ActorStatementFactory>();
+		this.actors = new HashMap<String, ActorStatementFactory>();
 	}
 	
 	public void buildActor(InputStream input) {
 		try {
 			ActorStatementFactory actorStatement = new ActorStatementFactory();
 			actorStatement.build(input);
-			actors.add(actorStatement);
+			actors.put(actorStatement.getMainActor().getName(), actorStatement);
 		} catch (IOException e) {
 			ErrorHandler.getErrorHandler().addError(new CustomError(e.getMessage()));
 		}
 	}
 	
 	public void verifyErrors() {
-		actors.forEach(actor -> actor.verifyAssociationErrors());
+		actors.values().forEach(actor -> actor.verifyAssociationErrors());
 	}
 	
 	public void print() {
-		actors.forEach(actor -> actor.print());
+		actors.values().forEach(actor -> actor.print());
 	}
 	
 	public String toXML() {
 		StringBuilder str = new StringBuilder();
 		
-		actors.forEach(actor -> str.append(actor.toXML()));
+		actors.values().forEach(actor -> str.append(actor.toXML()));
 		
 		XStream xstream = new XStream();
 		str.append(xstream.toXML(ErrorHandler.getErrorHandler().getErrors()));
