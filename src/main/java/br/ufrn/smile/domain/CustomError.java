6a2e640cb83a74447259119b93c2fd63390bec5f
Andreza Medeiros <andreza.cmedeiros@gmail.com>
2017-05-16 03:27:01 -0300

    add position to message

--- src/main/java/br/ufrn/smile/domain/CustomError.java
+++ src/main/java/br/ufrn/smile/domain/CustomError.java
@@ -19,31 +19,53 @@ public class CustomError {
 	}
 	
 	public void setAssociationError(Association association) {
+		this.position = association.getPosition();
+		
 		this.message = "filed association on " + fileName + ": " 
 						+ association.getType().getDescription() + " (" 
 						+ association.getActor().getType().getDescription() + " " 
 						+ association.getActor().getName() + ")";
+		
+		this.setPositionInMessage();
 	}
 	
 	public void setDependencyError(Dependency dependency) {
+		this.position = dependency.getPosition();
+		
 		this.message = "filed dependency on " + fileName + ": " 
 						+ dependency.getPerspective().getDescription() + " (" 
 						+ dependency.getActor().getName() + ") " 
 						+ dependency.getActor().getType().getDescription() + " for (" 
 						+ dependency.getType().getDescription() + " " 
 						+ dependency.getName() + ")";
+		
+		this.setPositionInMessage();
 	}
 	
 	public void setMissingActorError(Actor actor) {
+		this.position = actor.getPosition();
+		
 		this.message = "missing actor definition on " + fileName + ": "
 						+ actor.getType().getDescription() + " " 
 						+ actor.getName();
+		
+		this.setPositionInMessage();
 	}
 	
 	public void setFileNameError(String actorName) {
 		this.message = "the file " 
 						+ fileName + " should have the same name as the actor on it: " 
 						+ actorName;
+	}
+	
+	private void setPositionInMessage() {
+		StringBuilder sb = new StringBuilder(message);
+		sb.append(" at line " + position.getStartInLine() 
+					+ ":" + position.getStartCharPosition() 
+					+ " till line " + position.getStopInLine() 
+					+ ":" + position.getStopCharPosition());
+		
+		this.message = sb.toString();
 	}
 	
 	public String getFileName() {
