e5c580ec00248aa3d093270f18f8baa38d4b12a2
Andreza Medeiros <andreza.cmedeiros@gmail.com>
2017-05-17 15:09:31 -0300

    add warning to error handler

--- src/main/java/br/ufrn/smile/service/ErrorHandler.java
+++ src/main/java/br/ufrn/smile/service/ErrorHandler.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.ufrn.smile.domain.CustomError;
+import br.ufrn.smile.domain.CustomWarning;
 
 public class ErrorHandler {
 	private static final ErrorHandler errorHandler = new ErrorHandler();
 	private List<CustomError> errors = new ArrayList<CustomError>();
+	private List<CustomWarning> warnings = new ArrayList<CustomWarning>();
 	
 	private ErrorHandler() { }
 	
@@ -21,5 +23,13 @@ public class ErrorHandler {
 	
 	public List<CustomError> getErrors() {
 		return errors;
+	}
+
+	public void addWarning(CustomWarning warning) {
+		warnings.add(warning);
+	}
+
+	public List<CustomWarning> getWarnings() {
+		return warnings;
 	}
 }
